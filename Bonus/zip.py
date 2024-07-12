1d72516c47a3807e809c6ccc76d8c9f0a0d8c76c
Sandu Todica <sandu.todica@garmin.com>
2024-07-12 13:18:41 +0300

    Zip app completed
    Change-Id: Ie347aaf073f0b1e2948765b9fb21f173a787584a

--- Bonus/zip.py
+++ Bonus/zip.py
@@ -1,17 +1,31 @@
 import FreeSimpleGUI as sg
+from zip_creator import make_archive
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose", key="files")
 
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output", text_color="green")
 window = sg.Window("File compressor",
                    layout=[[label1, input1,choose_button1],
                            [label2, input2,choose_button2],
-                           [compress_button]])
-window.read()
+                           [compress_button, output_label]])
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case "Compress":
+            filepaths = values["files"].split(";")
+            folder = values["folder"]
+            make_archive(filepaths, folder)
+            window["output"].update(value="Compression completed!")
+        case sg.WINDOW_CLOSED:
+            break
+
 window.close()

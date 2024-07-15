d59013dd44df3043c923eab3698a31529d27dfbe
Sandu Todica <sandu.todica@garmin.com>
2024-07-15 13:22:05 +0300

    Adding Todo function
    Change-Id: I3d47c3a7d3aa227053a6f0f0bca1efb26f5fe5be

--- web.py
+++ web.py
@@ -2,6 +2,13 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My todo app")
 st.subheader("This is my todo application")
 st.write("This app is to improve my Python skills")
@@ -10,6 +17,8 @@ st.write("This app is to improve my Python skills")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo", placeholder="Add new todo...")
+st.text_input(label="Enter a todo", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
 
 print("Hello")
+st.session_state

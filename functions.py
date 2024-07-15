b6ac553e0418ac35c3c007e201cedf924fd129ee
Sandu Todica <sandu.todica@garmin.com>
2024-07-15 17:00:16 +0300

    First web app
    Change-Id: I6e88c2e554c299f9c2ad429f7d37000e237ff3d5

---
+++ functions.py
@@ -0,0 +1,16 @@
+DEFAULT_FILEPATH = "todo_list.txt"
+
+
+def get_todos(filepath=DEFAULT_FILEPATH):    # default argument
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=DEFAULT_FILEPATH):    # default argument should be at the end
+    """ Write the to-do items list in the next file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)

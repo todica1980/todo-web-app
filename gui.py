40c25472ebb00c1deb9795a077089afd241a098e
Sandu Todica <sandu.todica@garmin.com>
2024-07-15 13:01:26 +0300

    Create a static web interface
    Change-Id: I42beafafb8e28a13422e11a81f9f63229ee64e7b

--- gui.py
+++ gui.py
@@ -1,4 +1,3 @@
-import time
 
 import functions
 import FreeSimpleGUI
--- web.py
+++ web.py
@@ -2,25 +2,14 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
-
-def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
-
-
 st.title("My todo app")
 st.subheader("This is my todo application")
 st.write("This app is to improve my Python skills")
 
 
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox == True:
-        todos.pop(index)
-        functions.write_todos(todos)
-        del st.session_state[todo]
-        st.rerun()
+for todo in todos:
+    st.checkbox(todo)
 
-st.text_input(label="Enter a todo", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
+st.text_input(label="Enter a todo", placeholder="Add new todo...")
+
+print("Hello")
